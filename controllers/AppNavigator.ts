a038576a6a9b67b79dd50fe076d4607f38a55f83
efichot <fichotetienne@gmail.com>
2019-10-23 17:33:06 +0700

    Remove folder duplicate 2 ✅

--- controllers/AppNavigator.ts
+++
@@ -1,8 +0,0 @@
-import { createAppContainer, createSwitchNavigator } from "react-navigation";
-import MainNavigator from "../app/MainNavigator";
-
-export default createAppContainer(
-  createSwitchNavigator({
-    Main: MainNavigator
-  })
-);
